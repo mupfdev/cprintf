5817286c1b6b27a9b3743455357f8c065d93156b
null <60427892+vierofernando@users.noreply.github.com>
2021-10-10 11:16:35 +0700

    h

--- cprintf.h
+++ cprintf.h
@@ -1,9 +1,8 @@
 #ifndef CPRINTF_H
 #define CPRINTF_H
 
-#include <stdlib.h>
 #include <stdarg.h>
 
-void cprintf(const size_t fmt_am, const char * fmt, ...);
+void cprintf(const char * fmt, ...);
 
 #endif
